73fb0833d5e1b1e860be85a0cf4bc7c4d35edbd7
Alexander Somma <alexander.somma@gmail.com>
2011-03-07 15:19:43 -0500

    Added HelloWorld test app"

---
+++ HelloWorld/Classes/HelloWorldAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Alexander Somma on 11-03-07.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface HelloWorldAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Alexander Somma on 11-03-07.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"HelloWorldAppDelegate");
+    [pool release];
+    return retVal;
+}
